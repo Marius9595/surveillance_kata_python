ed85c81bc5bef5af44cbe59006c7fe62a1d55090
Mario S. Pinto Miranda <mario.pinto.miranda@gmail.com>
2023-03-06 16:58:27 +0000

    init

--- tests/test_narcissistic_number.py
+++ tests/test_narcissistic_number.py
@@ -2,16 +2,9 @@ from unittest import TestCase
 
 from assertpy import assert_that
 
-from src.narcissistic_number import is_narcissistic
 
-
-class TestIsNarcissisticNumber(TestCase):
-    def test_is_narcissistic_if_the_sum_of_its_digits_to_the_nth_power_equal_the_original_number(
+class Test(TestCase):
+    def test_foo(
         self,
     ):
-        assert_that(is_narcissistic(150)).is_false()
-        assert_that(is_narcissistic(0)).is_true()
-        assert_that(is_narcissistic(153)).is_true()
-
-    def test_narcissistic_numbers_should_be_positive(self):
-        assert_that(is_narcissistic).raises(ValueError).when_called_with(-1)
+        assert_that(False).is_false()
